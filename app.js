b9e3293b71baf8fb7d21c4fb60d1b3f5096f00ff
dinakajoy <odinakajoy909@gmail.com>
2025-03-20 18:25:27 +0100

    updated

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.65bot.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${process.env.DATABASE_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Successfully connected to DB!');
   })
